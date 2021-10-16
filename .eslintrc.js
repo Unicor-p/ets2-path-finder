387511442308636b582bfb2a11991588ac1513a0
JAGFx <contact@jagfx.fr>
2021-10-16 19:59:40 +0200

    :art: add code style checker and fixer

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,18 @@
 module.exports = {
-	env:           {
-		node: true
-	},
-	root: true,
-	parser: '@typescript-eslint/parser',
-	plugins: [
-		'@typescript-eslint',
-	],
-	extends: [
-		'eslint:recommended',
-		'plugin:@typescript-eslint/recommended',
-	],
-	rules: {
-		'no-mixed-spaces-and-tabs': [ 2, 'smart-tabs' ],
-		'@typescript-eslint/no-explicit-any': [1, { fixToUnknown: true }]
-	}
+  env: {
+    node: true
+  },
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  rules: {
+    'no-mixed-spaces-and-tabs': [2, 'smart-tabs'],
+    '@typescript-eslint/no-explicit-any': [1, { fixToUnknown: true }],
+    'prettier/prettier': 2
+  }
 };
